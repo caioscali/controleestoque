091e5a1241344cc3205337a92268dcb087b3c02a
caioscali <caioscali@gmail.com>
2020-02-25 07:52:46 -0300

    categoria

--- categoria.php
+++ categoria.php
@@ -12,14 +12,15 @@
 ?>
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Categorias</h1>
-    <div class="btn-toolbar mb-2 mb-md-0">
-    	<form class="col-auto ">
+    <form class="col-auto ">
 	    	<div class="form-group" method='post'>
                 <label>Descricao:</label>
                 <input type="text" class="form-control" id="descricao">
             </div>
              <input type="submit" name="cadastrar" class="btn btn-primary" value="Cadastrar"> 
         </form>
+    <div class="btn-toolbar mb-2 mb-md-0">
+    	
         <div class="btn-group mr-2"><!-- 
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Cadastrar Nova Categoria</button> -->
         </div>
