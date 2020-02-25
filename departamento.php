19e7ae065329fa2748c2ac0ff83de582db26dc0b
caioscali <caioscali@gmail.com>
2020-02-25 09:10:41 -0300

    departamento

--- departamento.php
+++ departamento.php
@@ -14,7 +14,19 @@
 ?>
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Categorias</h1>
-    <form class="col-auto " method="post">
+   
+    <div class="btn-toolbar mb-2 mb-md-0">
+        
+        <div class="btn-group mr-2">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Cadastrar Novo Departamento</button>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+             <form class="col-auto " method="post">
         <div class="form-group" >
             <label>nome:</label>
             <input type="text" class="form-control" id="nome" name="nome" >
@@ -25,10 +37,6 @@
         </div>
         <input type="submit" name="cadastrar" class="btn btn-primary" value="Cadastrar"> 
     </form>
-    <div class="btn-toolbar mb-2 mb-md-0">
-        
-        <div class="btn-group mr-2"><!-- 
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Cadastrar Nova Categoria</button> -->
         </div>
     </div>
 </div>
