68bdca8c0abebea8de32988ae5fee450541c9333
caioscali <caioscali@gmail.com>
2020-02-25 09:02:01 -0300

    categoria

--- categoria.php
+++ categoria.php
@@ -12,8 +12,8 @@
 ?>
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Categorias</h1>
-    <form class="col-auto ">
-	  	<div class="form-group" method='POST'>
+    <form class="col-auto " method="post">
+	  	<div class="form-group" >
             <label>Descricao:</label>
             <input type="text" class="form-control" id="descricao" name="descricao">
         </div>
