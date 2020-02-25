fef5f46d74a048657564b393d3bc8900a405273c
caioscali <caioscali@gmail.com>
2020-02-25 08:59:19 -0300

    categoria

--- categoria.php
+++ categoria.php
@@ -1,8 +1,8 @@
 <?php 
 	$categoria = new Categoria();
     //Cadastro de Categoria
-    if( isset($_POST['cadastrar'])):
-
+    if(isset($_POST['cadastrar'])):
+    	var_dump('oi');
     	$descricao = $_POST['descricao'];
     	$categoria->setDescricao($descricao);
 
@@ -13,12 +13,12 @@
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Categorias</h1>
     <form class="col-auto ">
-	    	<div class="form-group" method='post'>
-                <label>Descricao:</label>
-                <input type="text" class="form-control" id="descricao" name="descricao">
-            </div>
-             <input type="submit" name="cadastrar" class="btn btn-primary" value="Cadastrar"> 
-        </form>
+	  	<div class="form-group" method='post'>
+            <label>Descricao:</label>
+            <input type="text" class="form-control" id="descricao" name="descricao">
+        </div>
+        <input type="submit" name="cadastrar" class="btn btn-primary" value="Cadastrar"> 
+    </form>
     <div class="btn-toolbar mb-2 mb-md-0">
     	
         <div class="btn-group mr-2"><!-- 
