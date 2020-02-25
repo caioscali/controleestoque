397c47de436b6606d73ac9d6165d9ac500e9fc33
caioscali <caioscali@gmail.com>
2020-02-25 09:20:30 -0300

    departamento

--- departamento.php
+++ departamento.php
@@ -1,22 +1,22 @@
 <?php 
-    $departamento = new Departamento();
+$departamento = new Departamento();
     //Cadastro de Categoria
-    if(isset($_POST['cadastrar'])):
- 
-        $nome = $_POST['nome'];
-        $empresa = $_POST['empresa'];
-        $departamento->setNome($nome);
-        $departamento->setEmpresa($empresa);
+if(isset($_POST['cadastrar'])):
 
-        $departamento->insert();
+    $nome = $_POST['nome'];
+    $empresa = $_POST['empresa'];
+    $departamento->setNome($nome);
+    $departamento->setEmpresa($empresa);
 
-    endif;
+    $departamento->insert();
+
+endif;
 ?>
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Departamentos</h1>
-   
+
     <div class="btn-toolbar mb-2 mb-md-0">
-        
+
         <div class="btn-group mr-2">
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Cadastrar Novo Departamento</button>
         </div>
@@ -29,40 +29,51 @@
             <tr>
                 <th>Departamento:</th>
                 <th>Empresa:</th>
+                <th>Ação:</th>
             </tr>
         </thead>
         <?php
         $ordem = 'descricao';
         //$departamento = findAllDepartamento();
-         foreach ($departamento->findAll() as $key => $value) :
-        ?>
+        foreach ($departamento->findAll() as $key => $value) :
+            ?>
             <tbody>
                 <tr>
                     <td><?php echo $value->nome; ?></td>
                     <td><?php echo $value->empresa; ?></td>
-                </tr>
+                    <td>
+                        <?php echo "<a href='index.php?p=pessoa&acao=editar&id=" . $value->id . "'>Editar</a>"; ?>
+                        <?php
+                        if ($value->status == '1'){
+                            echo "<a href='index.php?p=pessoa&acao=inativar&id=" . $value->id ."'>Deletar</a>"; 
+                        }elseif ($value->status == '0'){
+                            echo "<a href='index.php?p=pessoa&acao=ativar&id=" . $value->id ."'>Inativo</a>";
+                        }
+                        ?>
+                    </td>
+                    </tr>
             </tbody>
 
-        <?php
-         endforeach; ?>
-
+    <?php
+        endforeach; 
+    ?>
     </table>
 </div>
 
-<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-           <form class="col-auto " method="post">
-            <div class="form-group" >
-                <label>Nome:</label>
-                <input type="text" class="form-control" id="nome" name="nome" >
-            </div>
-            <div class="form-group" >
-                <label>Empresa:</label>
-                <input type="text" class="form-control" id="empresa" name="empresa">
-            </div>
-            <input type="submit" name="cadastrar" class="btn btn-primary" value="Cadastrar"> 
-        </form>
+    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+             <form class="col-auto " method="post">
+                <div class="form-group" >
+                    <label>Nome:</label>
+                    <input type="text" class="form-control" id="nome" name="nome" >
+                </div>
+                <div class="form-group" >
+                    <label>Empresa:</label>
+                    <input type="text" class="form-control" id="empresa" name="empresa">
+                </div>
+                <input type="submit" name="cadastrar" class="btn btn-primary" value="Cadastrar"> 
+            </form>
+        </div>
     </div>
-</div>
 </div>
